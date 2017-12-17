8484d7dc31df26fa1d7338b8ef15bac8e4de227d
Павел <haletski.p@gmail.com>
2017-12-17 21:35:26 +0300

    a

--- GoProShop/ViewModels/FeedbackVM.cs
+++ GoProShop/ViewModels/FeedbackVM.cs
@@ -6,9 +6,11 @@ namespace GoProShop.ViewModels
     public class FeedbackVM : IdProvider
     {
         [Required(ErrorMessage = "Поле имя является обязательным")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Длина поля имя должна быть не меньше 3 и не больше 20 символов")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Поле фамилия является обязательным")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Длина поля фамилия должна быть не меньше 3 и не больше 20 символов")]
         public string SurName { get; set; }
 
         [Required(ErrorMessage = "Поле email является обязательным")]
@@ -17,6 +19,7 @@ namespace GoProShop.ViewModels
         public DateTime Date { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = "Поле сообщение является обязательным")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Длина поля сообщение должна быть не меньше 3 и не больше 250 символов")]
         public string Message { get; set; }
     }
 }
