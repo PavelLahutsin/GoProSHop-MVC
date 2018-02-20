76c44a68972ccc9e8c63434322dd04365af68cf9
Павел <haletski.p@gmail.com>
2018-02-21 00:05:24 +0300

    added functional tests for home page

---
+++ GoProShop.DAL.Tests1/FunctionalTests.cs
@@ -0,0 +1,226 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Text;
+using System.Threading;
+
+namespace GoProShop.DAL.Tests1
+{
+    public class FunctionalTests
+    {
+        private const string BaseUrl = "http://localhost:8888";
+        private HomePage _homePage;
+        private IWebDriver _driver;
+        
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _driver = new ChromeDriver("D:\\Apps");
+            _driver.Url = BaseUrl;
+            _homePage = new HomePage(_driver);
+        }
+
+        [Test]
+        public void Submit_Feedback_When_All_Values_Are_Correct_Display_Notification_Message()
+        { 
+            // Arrange
+            _homePage.InputUserName("testUser");
+            _homePage.InputSurname("testSurname");
+            _homePage.InputEmail("test@email.com");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage("testmessage");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsTrue(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_Values_Are_Not_Correct_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName(string.Empty);
+            _homePage.InputSurname(string.Empty);
+            _homePage.InputEmail(string.Empty);
+            _homePage.InputMessage(string.Empty);
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_UserName_Is_Too_Short_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName("tt");
+            _homePage.InputSurname("testSurname");
+            _homePage.InputEmail("test@email.com");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage("testmessage");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_UserName_Is_Too_Long_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName(GetInvalidMessage());
+            _homePage.InputSurname("testSurname");
+            _homePage.InputEmail("test@email.com");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage("testmessage");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_Surname_Is_Too_Short_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName("testUserName");
+            _homePage.InputSurname("tt");
+            _homePage.InputEmail("test@email.com");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage("testmessage");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_Surname_Is_Too_Long_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName("testUserName");
+            _homePage.InputSurname(GetInvalidMessage());
+            _homePage.InputEmail("test@email.com");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage("testmessage");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_Email_Is_Not_Valid_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName("testUserName");
+            _homePage.InputSurname("testSurName");
+            _homePage.InputEmail("te@dasdas");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage("testmessage");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_Rating_Is_Not_Chosen_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName("testUserName");
+            _homePage.InputSurname("testSurName");
+            _homePage.InputEmail("te@dasdas.com");
+            _homePage.InputMessage("testmessage");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_Message_Is_Too_Short_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName("testUserName");
+            _homePage.InputSurname("testSurName");
+            _homePage.InputEmail("te@dasdas");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage("tt");
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [Test]
+        public void Submit_Feedback_When_Message_Is_Too_Long_Not_Display_Notification_Message()
+        {
+            // Arrange
+            _homePage.InputUserName("testUserName");
+            _homePage.InputSurname("testSurName");
+            _homePage.InputEmail("te@dasdas.com");
+            _homePage.ClickStarRate5();
+            _homePage.InputMessage(GetInvalidMessage());
+
+            // Act
+            _homePage.SubmitFeedback();
+            Thread.Sleep(1000);
+
+            // Assert
+            Assert.IsFalse(_homePage.IsNotificationDisplayed());
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            _driver.Navigate().Refresh();
+        }
+
+        [OneTimeTearDown]
+        public void CloseBrowser()
+        {
+            _driver.Close();
+        }
+
+        private string GetInvalidMessage()
+        {
+            var sb = new StringBuilder("Test");
+
+            for (int i = 0; i < 100; i++)
+            {
+                sb.Append("Test");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ GoProShop.DAL.Tests1/HomePage.cs
@@ -0,0 +1,84 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace GoProShop.DAL.Tests1
+{
+    public class HomePage
+    {
+        private readonly IWebDriver _webDriver;
+        private readonly Actions _action;
+
+        public HomePage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+            _action = new Actions(webDriver);
+            
+            PageFactory.InitElements(webDriver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "Name")]
+        private IWebElement _feedbackUserName;
+
+        [FindsBy(How = How.Id, Using = "SurName")]
+        private IWebElement _feedbackSurName;
+
+        [FindsBy(How = How.Id, Using = "Email")]
+        private IWebElement _feedbackEmail;
+
+        [FindsBy(How = How.Id, Using = "Rate5")]
+        private IWebElement _feedbackRate5;
+
+        [FindsBy(How = How.Id, Using = "Message")]
+        private IWebElement _message;
+
+        [FindsBy(How = How.Id, Using = "submit-feedback")]
+        private IWebElement _submitFeedback;
+
+        [FindsBy(How = How.ClassName, Using = "table-notification-message")]
+        private IWebElement _notificationMessage;
+
+        
+        public void MoveToFeedbackUserName()
+        {
+            _action.MoveToElement(_feedbackUserName);
+            _action.Perform();
+        }
+
+        public void InputUserName(string value)
+        {
+            _feedbackUserName.SendKeys(value);
+        }
+
+        public void InputSurname(string value)
+        {
+            _feedbackSurName.SendKeys(value);
+        }
+
+        public void InputEmail(string value)
+        {
+            _feedbackEmail.SendKeys(value);
+        }
+
+        public void InputMessage(string value)
+        {
+            _message.SendKeys(value);
+        }
+
+        public void ClickStarRate5()
+        {
+            _feedbackRate5.Click();
+        }
+
+        public void SubmitFeedback()
+        {
+            _submitFeedback.Submit();
+        }
+
+        public bool IsNotificationDisplayed()
+        {
+           return _notificationMessage.Displayed;
+        }
+        
+    }
+}
