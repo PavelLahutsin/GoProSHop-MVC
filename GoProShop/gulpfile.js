90faa2e18f7511a9fee834e1ec1b82e106e7b030
Haletski <haletski.p@gmail.com>
2018-03-03 16:13:52 +0300

    added gulp file to project

---
+++ GoProShop/gulpfile.js
@@ -0,0 +1,15 @@
+﻿/// <binding BeforeBuild='sass' AfterBuild='sass' />
+var sass = require("gulp-sass");
+var gulp = require('gulp');
+
+var paths = {
+    webroot: "./Content/"
+}
+
+paths.scss = paths.webroot + "Sass/**/*.scss"; 
+
+gulp.task('sass', function () {
+    gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + "Css/"));
+});
